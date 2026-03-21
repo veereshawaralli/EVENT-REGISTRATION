9d91aaf0d23c3da0cd9177159cf55874bb12c293
veeresh <medicoveeresh8818@gmail.com>
2026-03-21 23:31:10 +0530

    fix: add EMAIL_USE_SSL and EMAIL_TIMEOUT to fix Render SMTP issues

--- config/settings.py
+++ config/settings.py
@@ -208,6 +208,8 @@ EMAIL_BACKEND = config(
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=False, cast=bool)
+EMAIL_TIMEOUT = config("EMAIL_TIMEOUT", default=10, cast=int)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="EventHub <noreply@eventhub.com>")
