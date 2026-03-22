6793f54d92227763efef68b7007e38f190889682
veeresh <medicoveeresh8818@gmail.com>
2026-03-22 16:09:26 +0530

    Fix missing imports in events/views.py causing deploy failure

--- events/views.py
+++ events/views.py
@@ -1,9 +1,13 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.paginator import Paginator
 from django.db.models import Count, Q, Sum
 from django.http import HttpResponse
-import csv
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.views.generic import CreateView, DeleteView, UpdateView
+import csv
 
 from .forms import EventForm, EventSearchForm
 from .models import Category, Event
